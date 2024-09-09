0ffe404ae3721701c2299748ffb158add2bc8e9a
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2024-09-09 10:14:57 -0300

    Implementação de renderização dos resultados a partir da base de dados

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log (dados)
+let section = document.getElementById("resultado-pesquisa")
+
+for (let dado of dados){
+    section.innerHTML += `
+        <div class="item-resultado">
+            <h2>${dado.nome}</h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <p class="descricao-meta"> ${dado.localizacao}</p>
+            <p class="descricao-meta"> ${dado.horarioFuncionamento}</p>
+            <a href=${dado.horarioFuncionamento} target="_blank">Clique aqui para saber mais</a>
+        </div>
+    `
+}
